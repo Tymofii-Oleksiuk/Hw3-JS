f1392a710c8d64e2344b9656780aeb5ae7e67d00
Tymofii-Oleksiuk <jji243372@gmail.com>
2024-09-23 14:42:27 +0200

    Hw3-Js

---
+++ index.js
@@ -0,0 +1,19 @@
+const result = 5 + 5 + "5";
+console.log(result);
+
+const email = "jji243372@gmail.com";
+console.log(email.includes("@"));
+
+const my = "My";
+const name = "name";
+const is = "is";
+
+const fullName = `${my} ${name} ${is} Victor`;
+console.log(fullName);
+
+const userName = 'Олександро';
+const payment = 'До сплати 300 гривень';
+
+const fullMessage = `Дякуємо ${userName}! ${payment}`
+
+console.log(alert(fullMessage));
